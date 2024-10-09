601b86f2be4b6965f7760868404098ea597c2f36
xdweb <xdweb.elyakouti@gmail.com>
2024-10-09 22:23:54 +0100

    by @xDweeb

---
+++ 0x01 F.ELArbi/0x02 Tps/0x01 TP 1/0x01 R/test.R
@@ -0,0 +1,8 @@
+print('Hello World')
+
+message <- function() {
+    print('Un autre message')
+}
+
+# Optionally, you can call the function after defining it
+message()
---
+++ 0x01 F.ELArbi/0x02 Tps/0x01 TP 1/0x02 Rstudio/test.R
@@ -0,0 +1,10 @@
+print('Hello World')
+
+print(5 + 5)
+
+message <- function() {
+  print('Un autre message')
+}
+
+# Optionally, you can call the function after defining it
+message()
