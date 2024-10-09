0f7a22d6a431a67cf3240cea57ef2acda5be220e
xdweb <xdweb.elyakouti@gmail.com>
2024-10-09 22:24:12 +0100

    by @xDweeb

---
+++ 0x01 F.ELArbi/0x02 Tps/0x02 TP 2/0x01 C.rstudio/TP_2.R
@@ -0,0 +1,12 @@
+#Exercice 1
+#1-cree un vecteur a contenant les nombres de 1 à 10 :
+a <- 1:10
+print(a)
+
+#2-Créer un vecteur b contenant les valeurs c(3, 5, 7, 9) :
+b <- c(3,5,7,9)
+b
+
+#verifie quels element de a sont supererieurs a 5:
+a_sup_5 <- a > 5
+print(a_sup_5)
