72b728557eaf7d5dbde28c897c469391f5f91e79
xdweb <xdweb.elyakouti@gmail.com>
2024-10-09 23:43:30 +0100

    by @xDweeb

---
+++ 0x01 F.ELArbi/0x02 Tps/0x03 TP 3/0x01 C.rstudio/TP_3.R
@@ -0,0 +1,25 @@
+#Exercice 1
+
+#1-Appeler les fonctions suivantes :
+sin(pi / 2)
+tan(pi / 4)
+log(10)
+round(3.14159, 2)
+cos(0)
+
+#Exercice 2 :
+#Développer un programme interactif :
+  
+#1-Lire le prénom de l'utilisateur :
+prenom <- readline(prompt = "Entrez votre prénom : ")
+cat("Bienvenue", prenom)
+
+#2-Lire une série de notes des modules :
+notes <- scan()
+cat("Les notes : ", notes)
+
+#3-Calculer la somme, la moyenne, et afficher :
+somme_notes <- sum(notes)
+moyenne_notes <- mean(notes)
+cat("Somme : ", somme_notes, " Moyenne : ", moyenne_notes)
+
